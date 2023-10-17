b94a58b0a1b84d676341faed79c9d91abc600faa
guise123 <71681809+guise123@users.noreply.github.com>
2023-10-17 21:12:01 +0800

    Update PWM.h

--- PWM.h
+++ PWM.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014 Anthony Hutchinson
+    Copyright (C) 2014 Anthony Hutchinson1
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
